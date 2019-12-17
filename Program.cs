145de872a3117687b15e72ffc05e45117ceceda6
unknown <Danish Ashraf>
2019-12-17 22:39:01 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_CircleCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
